b9de7f684ec20235d8e3bb2c4261c63da2d1e7a6
Fatemeh Eslaminasab <fatemeheslaminasab@Fatemehs-MacBook-Pro.local>
2025-01-13 17:31:14 -0500

    upadate modules

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.13.0"
+    }
+  }
+}
+
 provider "google" {
   project = "terraform-dev-001" # Your GCP project ID
   region  = "us-central1"       # Default region
