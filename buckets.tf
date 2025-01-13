ccb3cf7292e0886b22f4d727d5cc821464b7d9bb
Fatemeh Eslaminasab <fatemeheslaminasab@Fatemehs-MacBook-Pro.local>
2025-01-13 17:48:05 -0500

    upadate module

--- buckets.tf
+++ buckets.tf
@@ -1,6 +1,6 @@
 resource "google_storage_bucket" "tt-bucket" {
-  name          = "tt-bucket"
-  location      = "US"
+  name          = "trraform-dev-001-bucket"
+  location      = "us-central1"
   force_destroy = true
 
   public_access_prevention = "enforced"
